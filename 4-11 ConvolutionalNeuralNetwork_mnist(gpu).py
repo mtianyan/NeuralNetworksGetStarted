324c23919f903906193a8b1bf8a8342193c79411
mtianyan <1147727180@qq.com>
2018-04-04 18:16:31 +0800

    学习lTensorFlow常量变量，回填值，默认图等

--- 4-11 ConvolutionalNeuralNetwork_mnist(gpu).py
+++ 4-11 ConvolutionalNeuralNetwork_mnist(gpu).py
@@ -279,26 +279,27 @@ if __name__ == '__main__':
     #     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
     # net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
 
-    # 加入卷积池化层
-    net = Network([
-        ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
-                      filter_shape=(20, 1, 5, 5),
-                      poolsize=(2,2)),
-        FullyConnectedLayer(n_in=20*12*12, n_out=100),
-        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
-    net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
-
-    # 多加入一层卷积池化层
+    # # 加入卷积池化层
     # net = Network([
     #     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
     #                   filter_shape=(20, 1, 5, 5),
     #                   poolsize=(2,2)),
-    #     ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
-    #                   filter_shape=(40, 20, 5, 5),
-    #                   poolsize=(2,2)),
-    #     FullyConnectedLayer(n_in=40*4*4, n_out=100),
+    #     FullyConnectedLayer(n_in=20*12*12, n_out=100),
     #     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
-    #
+    # net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+
+    # 多加入一层卷积池化层
+    net = Network([
+        ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                      filter_shape=(20, 1, 5, 5),
+                      poolsize=(2,2)),
+        ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                      filter_shape=(40, 20, 5, 5),
+                      poolsize=(2,2)),
+        FullyConnectedLayer(n_in=40*4*4, n_out=100),
+        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+    net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+
     # # 使用Relu替代sigmoid
     # net = Network([
     #     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
